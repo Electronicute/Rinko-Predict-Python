5429f5c16db90be2e132b38dbd8f12ca662c6180
Hanametro <61606826+Hanametro@users.noreply.github.com>
2020-10-06 14:04:57 +0800

    ...

--- _main.py
+++ _main.py
@@ -1,7 +1,7 @@
 import os
 import DataRef
 import GraphDraw
-
+import time
 imgbasePath='D:/asd/now/'
 basePath='D:/asd/'
 
@@ -9,9 +9,9 @@ basePath='D:/asd/'
 #basePath='/root/web/rinkoapi/ycxcore/'
 
 areacode=3          #country Code
-TogglePara = False  #if PredNow please to change to True
+TogglePara = True  #if PredNow please to change to True
 #test only
-Bnum=52           #eventNumber
+Bnum=70          #eventNumber
     
     
 #make sure of Dir is create
@@ -21,6 +21,11 @@ if not os.path.exists(basePath):
     os.mkdir(basePath)
 if not os.path.exists(basePath+str(areacode)):
     os.mkdir(basePath+str(areacode))
-    
-DataRef.GetDataStorage(basePath,areacode,TogglePara,Bnum)             #make sure that we've already storage the file
-GraphDraw.GetDataPic(areacode,imgbasePath,basePath,TogglePara,Bnum)   #use file to Pic
+
+while True:
+    try:
+        DataRef.GetDataStorage(basePath,areacode,TogglePara,Bnum)             #make sure that we've already storage the file
+        GraphDraw.GetDataPic(areacode,imgbasePath,basePath,TogglePara,Bnum)   #use file to Pic
+    except:
+        print('FAIL')
+    time.sleep(300)
